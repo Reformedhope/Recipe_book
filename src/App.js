86dde1e0b0f9e433196cf7307f2bca821837d182
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-08-01 22:13:50 -0400

    Added stylization to the footer as well as to the background image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import RecipePage from "./Pages/RecipePage";
-
+import Footer from "./Components/Footer/Footer";
+import './app.css'
 function App() {
   return (
-    <div>
+    <div className="background">
+      <div className=" container">
       <Router>
         <Routes>
           <Route path="/" element = {<HomePage/>}/>
           <Route path ="/recipes" element = {<RecipePage/>}/>
         </Routes>
       </Router>
-      
+      </div>
+      <Footer/>
     </div>
+    
   );
 }
 
