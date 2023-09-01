6db434d4a4671db57e3f224413cb75c296fc6dc6
Steph <steph_salzburg@hotmail.com>
2023-08-31 21:20:52 -0400

    added a pull request to the nutrients page

---
+++ src/Pages/NutritionPage.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Nutrition = (props) => {
+    const [nutrients, setNutrients]=useState
+    async function() {
+        let response = await axios.get(``)
+    }
+
+
+    return ( 
+    
+    <div>
+
+    </div>
+     );
+}
+ 
+export default Nutrition;
