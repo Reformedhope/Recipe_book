f4d1c5499023ff4dcd601a97f2d23a9dce60a983
reformedhope <steph_salzburg@hotmail.com>
2024-04-13 21:44:11 -0400

    fixed e navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import RecipePage from "./Pages/RecipePage/RecipePage";
+import PersonalRecipePage from "./Pages/PersonalRecipePage/PersonalRecipePage";
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/Navbar/NavBar";
-import "./app.css";
+import './app.css'
 function App() {
-
   return (
-
     <div className="background">
       <div className=" container">
-        <NavBar />
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/recipes" element={<RecipePage />} />
-          </Routes>
-        </Router>
+        
+
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element = {<HomePage/>}/>
+          <Route path ="/recipes" element = {<RecipePage/>}/>
+          <Route path ="/PersonalRecipePage" element = {<PersonalRecipePage/>}/>
+        </Routes>
+      </Router>
       </div>
-      <Footer />
+      <Footer/>
     </div>
     
   );
---
+++ src/Pages/PersonalRecipePage/PersonalRecipePage.jsx
@@ -0,0 +1,14 @@
+
+
+
+const PersonalRecipePage  = () => {
+
+    return ( 
+        <div>
+            <p> This is your personal recipe page</p>
+            {/* ! TODO:  create a data base to hold recipes( look into what would best work for this) */}
+        </div>
+     );
+}
+ 
+export default PersonalRecipePage ;
